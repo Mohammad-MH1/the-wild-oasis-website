57e5ac4b31213ec200e4db6e0fc272b2b06e4142
Mohammad Mohammadi <mohammadyahoo15@gmail.com>
2024-11-09 16:38:33 +0330

    Add metadata for pages and favicon

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,16 @@ import { ReactNode } from 'react';
 
 import '@/app/_styles/globals.css';
 import Logo from '@/app/_components/Logo';
+import { title } from 'process';
+
+export const metadata = {
+  title: {
+    template: '%s | The Wild Oasis',
+    default: 'Welcome | The Wild Oasis',
+  },
+  description:
+    'Luxurious cabin hotel, located in the heart of the italian Dolomites, surrounded by beautiful mountains and dark forests',
+};
 
 function RootLayout({ children }: { children: ReactNode }) {
   return (
